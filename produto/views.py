c3ab4fd84f66dbdf74c3baa17e09fdc9eeebb2ac
camargo <gui.ferreira.camargo@gmail.com>
2024-03-18 16:33:19 -0300

    Criado template do produto unitário e variações

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from .models import Produto
 
@@ -11,8 +12,11 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    ...
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
