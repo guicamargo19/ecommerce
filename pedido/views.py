80edc42154dcd82735ef5e334c6a66a37ca4bc24
camargo <gui.ferreira.camargo@gmail.com>
2024-03-20 17:05:17 -0300

    Alterado view Pagar para SalvarPedido

--- pedido/views.py
+++ pedido/views.py
@@ -9,6 +9,11 @@ from utils import utils
 
 
 class Pagar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+
+class SalvarPedido(View):
     template_name = 'pedido/pagar.html'
 
     def get(self, *args, **kwargs):
@@ -92,11 +97,6 @@ class Pagar(View):
 
         del self.request.session['carrinho']
         return redirect('pedido:lista')
-
-
-class SalvarPedido(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('SalvarPedido')
 
 
 class Detalhe(View):
